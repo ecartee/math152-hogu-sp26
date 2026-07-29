cb41739c2c834481d8c5f381a7a458b47b84b4f8
Elliot Cartee <evc34@cornell.edu>
2026-07-29 09:34:17 -0500

    Latest build deployed.

---
+++ session1/lunr-pretext-search-index.js
@@ -0,0 +1,167 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "session1wks",
+  "level": "1",
+  "url": "#session1wks",
+  "type": "Article",
+  "number": "",
+  "title": "MATH 152 Hands-On, Grades Up",
+  "body": " MATH 152 Hands-On, Grades Up   For workspace between exercises, print this worksheet or download to your device by clicking the printer icon.    Session #1: The Substitution Rule & Area Between Curves -- Sections 5.5 & 6.1     Warm-Up  Answer each of the following as best you can. Feel free to discuss your thoughts with peers around you.     Suppose you want to use the substitution rule to evaluate the integral How would you rank the following choices of substitution?   ,  ,  ,  ,           Best choice  Can work  Won't work      Substitution              Given and , shade the region, , bounded by the two functions. Set up, but do not evaluate , the integral that would find the area of .    Graph of two parabolas on a coordinate grid. The parabola f of x equals x squared opens upward with vertex at the origin. The parabola g of x equals 2 x minus x squared opens downward with vertex at the point (1, 1). The two curves intersect at the origin and at the point (1, 1).           Directions: For each of the integrals below, categorize them as one of the following:    Substitution is not necessary, this can be solved using basic rules of integration (e.g., power, basic trig, etc.)  Can be solved using substitution  neither A nor B.    For the ones in which you answer (B), identify the expression to substitute for and then write the whole integral in terms of and . It is not necessary to evaluate the integrals for the warm-up of this session.                                         Let's get our hands on!  Answer each of the following as best you can. Feel free to discuss your thoughts with peers around you.     Evaluate the following indefinite integrals:                     Evaluate the following definite integrals:                     Evaluate the following indefinite integrals (some of these are a little trickier):                     A world-class sprinter runs a 100-meter race while wearing a device that tracks their velocity as a function of time. After running the race, the device reports back that their velocity (in meters per second) as a function of time (in seconds since the start of the race) can be represented by the function:     Use a definite integral to find the distance that the sprinter has ran after seconds.    How long (in seconds) did it take for the sprinter to finish the 100-meter race?         Given and ,     Sketch the region enclosed by the two curves.    Blank coordinate grid for sketching, with x-axis from negative 4 to 4 and y-axis from negative 2 to 4.       Find the area of this region .      Given the curves and ,     Sketch the region enclosed by these curves for .    Blank coordinate grid for sketching, with x-axis from negative 1 to 5 and y-axis from negative 2 to 2.       Find the area of this region .         Given , , and , answer the questions that follow:     Sketch the triangular region, , bounded by the three functions.    Blank coordinate grid for sketching, with x-axis from negative 1 to 5 and y-axis from negative 1 to 5.       Find the points of the vertices of the triangle.    Set up but do not evaluate an integral (or integrals) that gives the area of .         Consider the shaded region in the graph below. Use an integral (or integrals) to find the area of the shaded region.    Shaded parallelogram-shaped region on a coordinate grid, bounded above by the horizontal line y equals 4, below by the horizontal line y equals 2, on the left by the line y equals 3 x plus 8, and on the right by the line y equals 2 x minus 2. The vertices of the region are at (negative 2, 2), (2, 2), (3, 4), and (negative 4 thirds, 4).        Consider the shaded region in the graph below. Use an integral (or integrals) to find the area of the shaded region.    Shaded region enclosed by two sideways parabolas: the curve x equals y squared minus 5 y opens to the right, and the curve x equals 3 y minus y squared opens to the left. The two curves intersect at the origin and at the point (negative 4, 4), and the enclosed region lies mostly in the second quadrant.          Grades Up!  Choose the best answer for each of the following.     When using the substitution method to evaluate the indefinite integral which is the best choice for the substitution ?               Which integral would find the area of the region enclosed by , , and ?                Evaluate the following integral:                 Evaluate the following integral:                Challenge  Evaluate the following integral and interpret it as the area of a region. Sketch the region the integral represents.     ( Hint : Rewrite as a piecewise defined function. To do so, you'll need to find where .)     "
+},
+{
+  "id": "session1wks-3-2-2",
+  "level": "2",
+  "url": "#session1wks-3-2-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Suppose you want to use the substitution rule to evaluate the integral How would you rank the following choices of substitution?   ,  ,  ,  ,           Best choice  Can work  Won't work      Substitution           "
+},
+{
+  "id": "session1wks-3-2-3",
+  "level": "2",
+  "url": "#session1wks-3-2-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Given and , shade the region, , bounded by the two functions. Set up, but do not evaluate , the integral that would find the area of .    Graph of two parabolas on a coordinate grid. The parabola f of x equals x squared opens upward with vertex at the origin. The parabola g of x equals 2 x minus x squared opens downward with vertex at the point (1, 1). The two curves intersect at the origin and at the point (1, 1).     "
+},
+{
+  "id": "session1wks-3-3-1",
+  "level": "2",
+  "url": "#session1wks-3-3-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Directions: For each of the integrals below, categorize them as one of the following:    Substitution is not necessary, this can be solved using basic rules of integration (e.g., power, basic trig, etc.)  Can be solved using substitution  neither A nor B.    For the ones in which you answer (B), identify the expression to substitute for and then write the whole integral in terms of and . It is not necessary to evaluate the integrals for the warm-up of this session.                                    "
+},
+{
+  "id": "session1wks-3-4-2",
+  "level": "2",
+  "url": "#session1wks-3-4-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Evaluate the following indefinite integrals:               "
+},
+{
+  "id": "session1wks-3-5-1",
+  "level": "2",
+  "url": "#session1wks-3-5-1",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Evaluate the following definite integrals:               "
+},
+{
+  "id": "session1wks-3-6-1",
+  "level": "2",
+  "url": "#session1wks-3-6-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Evaluate the following indefinite integrals (some of these are a little trickier):               "
+},
+{
+  "id": "session1wks-3-7-1",
+  "level": "2",
+  "url": "#session1wks-3-7-1",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  A world-class sprinter runs a 100-meter race while wearing a device that tracks their velocity as a function of time. After running the race, the device reports back that their velocity (in meters per second) as a function of time (in seconds since the start of the race) can be represented by the function:     Use a definite integral to find the distance that the sprinter has ran after seconds.    How long (in seconds) did it take for the sprinter to finish the 100-meter race?   "
+},
+{
+  "id": "session1wks-3-8-1",
+  "level": "2",
+  "url": "#session1wks-3-8-1",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  Given and ,     Sketch the region enclosed by the two curves.    Blank coordinate grid for sketching, with x-axis from negative 4 to 4 and y-axis from negative 2 to 4.       Find the area of this region .   "
+},
+{
+  "id": "session1wks-3-8-2",
+  "level": "2",
+  "url": "#session1wks-3-8-2",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "  Given the curves and ,     Sketch the region enclosed by these curves for .    Blank coordinate grid for sketching, with x-axis from negative 1 to 5 and y-axis from negative 2 to 2.       Find the area of this region .   "
+},
+{
+  "id": "session1wks-3-9-1",
+  "level": "2",
+  "url": "#session1wks-3-9-1",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "  Given , , and , answer the questions that follow:     Sketch the triangular region, , bounded by the three functions.    Blank coordinate grid for sketching, with x-axis from negative 1 to 5 and y-axis from negative 1 to 5.       Find the points of the vertices of the triangle.    Set up but do not evaluate an integral (or integrals) that gives the area of .   "
+},
+{
+  "id": "session1wks-3-10-1",
+  "level": "2",
+  "url": "#session1wks-3-10-1",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "  Consider the shaded region in the graph below. Use an integral (or integrals) to find the area of the shaded region.    Shaded parallelogram-shaped region on a coordinate grid, bounded above by the horizontal line y equals 4, below by the horizontal line y equals 2, on the left by the line y equals 3 x plus 8, and on the right by the line y equals 2 x minus 2. The vertices of the region are at (negative 2, 2), (2, 2), (3, 4), and (negative 4 thirds, 4).     "
+},
+{
+  "id": "session1wks-3-10-2",
+  "level": "2",
+  "url": "#session1wks-3-10-2",
+  "type": "Worksheet Exercise",
+  "number": "9",
+  "title": "",
+  "body": "  Consider the shaded region in the graph below. Use an integral (or integrals) to find the area of the shaded region.    Shaded region enclosed by two sideways parabolas: the curve x equals y squared minus 5 y opens to the right, and the curve x equals 3 y minus y squared opens to the left. The two curves intersect at the origin and at the point (negative 4, 4), and the enclosed region lies mostly in the second quadrant.     "
+},
+{
+  "id": "session1wks-3-11-2",
+  "level": "2",
+  "url": "#session1wks-3-11-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  When using the substitution method to evaluate the indefinite integral which is the best choice for the substitution ?            "
+},
+{
+  "id": "session1wks-3-11-3",
+  "level": "2",
+  "url": "#session1wks-3-11-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Which integral would find the area of the region enclosed by , , and ?          "
+},
+{
+  "id": "session1wks-3-12-1",
+  "level": "2",
+  "url": "#session1wks-3-12-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Evaluate the following integral:              "
+},
+{
+  "id": "session1wks-3-12-2",
+  "level": "2",
+  "url": "#session1wks-3-12-2",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Evaluate the following integral:           "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
